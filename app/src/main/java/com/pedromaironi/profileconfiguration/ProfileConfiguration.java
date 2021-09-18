f923a38ed3df6bc825568e6601ca1b34a564fb25
Pedro Toribio <pedrom_t@hotmail.com>
2021-09-18 23:27:44 +0200

    [Profile] Page configuration of user

---
+++ app/src/main/java/com/pedromaironi/profileconfiguration/ProfileConfiguration.java
@@ -0,0 +1,16 @@
+package com.pedromaironi.profileconfiguration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileConfiguration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
