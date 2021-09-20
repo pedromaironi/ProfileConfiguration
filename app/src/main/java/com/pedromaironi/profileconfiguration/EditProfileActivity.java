93310fb7d809cfb2995a85e0dec02bfdd22ca255
Pedro Toribio <pedrom_t@hotmail.com>
2021-09-20 20:17:54 +0200

    [Edit profile] Adding inputs fields

---
+++ app/src/main/java/com/pedromaironi/profileconfiguration/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.pedromaironi.profileconfiguration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+
+}
--- app/src/main/java/com/pedromaironi/profileconfiguration/ProfileConfiguration.java
+++ app/src/main/java/com/pedromaironi/profileconfiguration/ProfileConfiguration.java
@@ -1,15 +1,33 @@
 package com.pedromaironi.profileconfiguration;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileConfiguration extends AppCompatActivity {
 
+//    TextView editProfilebtn;
+    Button editProfilebtn;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editProfilebtn = (Button) findViewById(R.id.EditProfileButton1);
+
+        editProfilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileConfiguration.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
