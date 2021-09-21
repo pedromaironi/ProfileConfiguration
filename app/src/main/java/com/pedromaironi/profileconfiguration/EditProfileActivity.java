4626bb0f0f4e63b1bb86914ab7f01be7b60f7a79
Pedro Toribio <pedrom_t@hotmail.com>
2021-09-21 22:07:20 +0200

    [Edit profile] Adding all styles

--- app/src/main/java/com/pedromaironi/profileconfiguration/EditProfileActivity.java
+++ app/src/main/java/com/pedromaironi/profileconfiguration/EditProfileActivity.java
@@ -2,14 +2,28 @@ package com.pedromaironi.profileconfiguration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    ImageView backArrowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        backArrowButton = (ImageView) findViewById(R.id.backArrowButton);
+        backArrowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EditProfileActivity.this, ProfileConfiguration.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
